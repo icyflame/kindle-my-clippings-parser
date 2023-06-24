d2dd39033f7d2b396ee34d4ad22b4fe9a8223805
Siddharth Kannan <mail@siddharthkannan.in>
2023-06-24 16:36:12 +0900

    Flush remaining input to the output buffer

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -9,22 +9,26 @@ import (
 )
 
 // MakePlaintextEmailFromClipping ...
-func MakePlaintextEmailFromClipping(c parser.Clipping) string {
+func MakePlaintextEmailFromClipping(c parser.Clipping) (string, error) {
 	var buffer bytes.Buffer
 	w := textutil.NewUTF8WrapWriter(&buffer, 80)
 	if _, err := w.Write([]byte(c.Text)); err != nil {
-		fmt.Println("there was an error: %v", err)
+		return "", fmt.Errorf("could not make plaintext email > %w", err)
+	}
+
+	if err := w.Flush(); err != nil {
+		return "", fmt.Errorf("could not flush all input to output buffer > %w", err)
 	}
 
 	return fmt.Sprintf(`Today's Excerpt
 
 Today's excerpt is a highlight created on %s.
 
-> %s
+%s
 
--- %s (p.%s)
+-- %s
 
 `,
-		c.CreateTime.Format("2006-01-02"), c.Text, c.Source, c.Page,
-	)
+		c.CreateTime.Format("2006-01-02"), buffer.String(), c.Source,
+	), nil
 }
