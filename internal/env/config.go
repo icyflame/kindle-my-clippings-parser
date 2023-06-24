2b0378216f297721049bf9c7ab2e63c0a2fd29cb
Siddharth Kannan <mail@siddharthkannan.in>
2023-06-24 16:36:26 +0900

    Use libraries to parse the environment

---
+++ internal/env/config.go
@@ -0,0 +1,28 @@
+package env
+
+import (
+	"fmt"
+
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Environment struct {
+	SendgridAPIKey string `envconfig:"SENDGRID_API_KEY" required:"true"`
+	SenderName     string `envconfig:"sender_name"`
+	SenderEmail    string `envconfig:"sender_email"`
+	ReceiverName   string `envconfig:"receiver_name"`
+	ReceiverEmail  string `envconfig:"receiver_email"`
+}
+
+// Process ...
+func Process() (Environment, error) {
+	godotenv.Load()
+	var environ Environment
+	err := envconfig.Process("", &environ)
+	if err != nil {
+		return Environment{}, fmt.Errorf("could not process environment variables > %w", err)
+	}
+
+	return environ, nil
+}
